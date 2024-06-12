8a75203ac6a0f92901d5cb8668c49a6c53e46acd
parth kapoor <kapoorparth096@gmail.com>
2024-06-12 16:31:23 +0530

    done

--- src/website/Home.jsx
+++ src/website/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
                 <section className="header d-flex align-items-center">
                     <div className="row">
                         <div className="col-12 col-md-6 col-xl-6 temp-1 d-flex flex-column justify-content-center order-2 order-lg-1 px-4">
-                            <h1 className="fw-bolder fs-1 lh-base">React : Famous and Powerful <span>UI Library</span> for frontend</h1>
+                            <h1 className="fw-bolder fs-1 lh-base lg-heading">React : Famous and Powerful <span>UI Library</span> for frontend</h1>
                             <h4 className="fw-bolder mt-3">I am full stack web <span>developer</span></h4>
                             <p className="sm-text fw-light desc my-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. In eum dolor tempora inventore quos suscipit voluptate accusantium consequatur ullam amet quibusdam debitis ratione, iste odio quidem placeat deleniti maxime minima libero quisquam ex id deserunt ipsam nam. Delectus praesentium ratione laudantium temporibus, nesciunt quaerat voluptate fuga molestiae rerum nostrum tenetur.</p>
                             <div className="btns d-flex gap-5 mb-5">
--- src/website/Navbar.jsx
+++ src/website/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
         <nav className="navbar nav-bg navbar-expand-lg navbar-light bg-light py-4">
           <div className="container-fluid">
             <h1 className='fs-4 fw-bold'>
-              <NavLink className="navbar-brand" to="/react-1">Parth <span id="my-name">Kapoor</span></NavLink>
+              <NavLink className="navbar-brand" to="/react-1">Parth <span id="my-name" className='my-logo'>Kapoor</span></NavLink>
             </h1>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
@@ -14,13 +14,14 @@ function Navbar() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <NavLink className="nav-link" aria-current="page" to="/react-1">Home</NavLink>
+                  <NavLink className= {({isActive})=> isActive ? " nav-link is-active" : "nav-link not-active"}  to="/react-1/homepage">Home</NavLink>
+                </li>
+                
+                <li className="nav-item">
+                  <NavLink className= {({isActive})=> isActive ? " nav-link is-active" : "nav-link not-active "} to="/react-1/weather">Check Weather</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" aria-current="page" to="/react-1/weather">Check Weather</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/react-1/form">Contact Us</NavLink>
+                  <NavLink className= {({isActive})=> isActive ? " nav-link is-active" : " nav-link not-active"} to="/react-1/form">Contact Us</NavLink>
                 </li>
 
               </ul>
